a35993eb7fb72431b353191d56dae5428593b537
Pedro Revilla <peterevilla@gmail.com>
2020-09-25 21:14:09 -0500

    hit mvp

--- adv.py
+++ adv.py
@@ -11,10 +11,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -31,7 +31,7 @@ traversal_path = []
 unexplored = set()
 def traversal(player):
 
-    
+    #Traverse the maze
     visited = set()
     queue = deque()
     queue.append([("s", player.current_room.id)])
